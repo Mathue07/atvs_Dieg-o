05f84406434a310f2be434d382bba1c2b3839aae
mathue07 <miteuxbit@gmail.com>
2026-05-29 11:25:25 -0300

    aula01

--- aula01/ex3.js
+++ aula01/ex3.js
@@ -1,8 +1,17 @@
 const input = require("readline-sync")
 
 let qnt = 5
+let numeros
+let total = 0
 
 for(let i = 0; i <= 4;){
-    let numeros = Number(input.question(`Escreva ${qnt} numero(s):`))
+    numeros = Number(input.question(`Escreva ${qnt} numero(s):`))
+    total = total + numeros
     i++; qnt--
 }
+
+let media = (total / 5);
+
+console.clear();
+
+console.log(`A sua media eh: ${media}`);
