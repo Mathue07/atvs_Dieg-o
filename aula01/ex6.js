fa5f212f309efd877865afb7d0b44f1f7fc4431f
mathue07 <miteuxbit@gmail.com>
2026-05-30 19:55:11 -0300

    aula01

--- aula01/ex6.js
+++ aula01/ex6.js
@@ -1,2 +1,9 @@
 const input = require("readline-sync");
 
+const secret = 4763
+let erros
+let tentativa
+
+while(true){
+    tentativa = console.log(input.question("Digite o numero suuuuuper secreto: ")) +
+}
