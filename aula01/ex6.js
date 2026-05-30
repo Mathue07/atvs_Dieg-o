46743eaf0ccf35fac5310175d3a82b98a74fe5d4
mathue07 <miteuxbit@gmail.com>
2026-05-30 20:18:03 -0300

    aula01

--- aula01/ex6.js
+++ aula01/ex6.js
@@ -1,9 +1,22 @@
 const input = require("readline-sync");
 
-const secret = 4763
-let erros
-let tentativa
+const secreto = 47
+let tentativas = 0
+let chute = 0
 
 while(true){
-    tentativa = console.log(input.question("Digite o numero suuuuuper secreto: ")) +
+    chute = input.question("Digite o numero suuuuuper secreto: ")
+    tentativas = tentativas + 1;
+
+    if(chute == secreto){
+        console.log(`Parabens! Voce acertou o numero secreto em ${tentativas} tentativas!`)
+    break;
+    }
+    else if(chute > secreto){
+        console.log("O numero secreto eh menor")
+    }
+    else{
+        console.log("O numero secreto eh maior")
+    }
 }
+
