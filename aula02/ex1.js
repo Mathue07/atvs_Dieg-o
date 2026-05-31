d7626ddbfbd2906a6188a3af444a3473f18063c0
mathue07 <miteuxbit@gmail.com>
2026-05-31 19:51:24 -0300

    aula02

--- aula02/ex1.js
+++ aula02/ex1.js
@@ -1,5 +1,36 @@
-const input = require("readline-sync");
+const input = require('readline-sync');
 
-let nome = readline("Qual seu nome? ")
+let n1
+let n2
 
-console.log(`Meu nome é ${nome}`);
+n1 = Number(input.question("Digite o primeiro Num: "))
+n2 = Number(input.question("Digite o segundo: "))
+
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    if (b === 0) {
+        return "Erro: divisão por zero";
+    }
+    return a / b;
+}
+
+let soma = somar(n1, n2);
+let sub = subtrair(n1, n2);
+let mult = multiplicar(n1, n2);
+let div = dividir(n1, n2);
+
+console.log("Soma: " + soma);
+console.log("Subtração: " + sub);
+console.log("Multiplicação: " + mult);
+console.log("Divisão: " + div);
