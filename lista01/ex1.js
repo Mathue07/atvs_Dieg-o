93a3d991e75d75b935c793985a6eb48a63410107
mathue07 <miteuxbit@gmail.com>
2026-06-28 18:46:32 -0300

    Lista01 - exercicios

--- lista01/ex1.js
+++ lista01/ex1.js
@@ -1,6 +1,6 @@
 const input = require('readline-sync');
 
-let C = input.question(`Digite uma temperatura em graus celcius: `)
+let C = input.question(`Digite a temperatura em graus celcius: `)
 let F = C * 1.8 + 32
 
-console.log(`asasas ${F}`)
+console.log(`A temperatura em Fahrenheit é: ${F}`)
