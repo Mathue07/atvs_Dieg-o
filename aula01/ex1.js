19cfbd908288e1f72eca914a1550c2be2c8405c3
mathue07 <miteuxbit@gmail.com>
2026-06-28 18:45:08 -0300

    Aula JS - Introdução a sintaxe

--- aula01/ex1.js
+++ aula01/ex1.js
@@ -4,4 +4,4 @@ let nome = input.question("Escreva seu nome: ")
 let idade = input.question("Quantos anos voce tem?: ")
 let cidade = input.question("Em que cidade voce mora?: ")
 
-console.log(`Olá, sou ${nome}, tenho ${idade} e moro em ${cidade}`)
+console.log(`Oi, sou ${nome}, tenho ${idade} e moro em ${cidade}`)
