59a1b7c83dd8ab7141a42a2d46d33a00c71da6d1
mathue07 <miteuxbit@gmail.com>
2026-07-09 11:34:52 -0300

    aula06 - CRUD completo em memoria

--- aula06/ex1.js
+++ aula06/ex1.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { error } = require("node:console");
+const {error} = require("node:console");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/inicio', (req, res) => {
 
     res.json({"mensagem de cu sujo": " Meu servidor NÃO esta no ar!"});
     }
@@ -14,34 +14,18 @@ app.get('/', (req, res) => {
 app.get('/inicio', (req, res) => {
 
     console.log("Olá! Bem vindo a esse cu do mundo, um site onde você não pode fazer bosta nenhuma. Aproveite!")
-
-    console.log("Nossas abas:    http://localhost3000/pintos-de-borracha;     http://localhost3000/")
     
 })
 
 const lanches = [
-        {id: 1, nome: "Enroladinho de sasixa", preço: 5.99},
-        {id: 2, nome: "Hamburgão simples", preço: 7.50},
-        {id: 3, nome: "Coxinha de costela", preço: 6.00}
-]
+        {id: "1", nome: "Pastel de Pizza", preço: 6.59},
+        {id: "2", nome: "Risoli de carne", preço: 5.00},
+        {id: "3", nome: "Esfiha de Carne", preço: 10.00}
+    ];
 
 app.get('/lanches', (req, res) => {
-    
+    res.json(lanches)
 })
-
-app.post(’/lanches’, (req, res) => {
-
-    const novoAluno = {
-        id : proximoId ,
-        nome : req . body . nome ,
-        nota : req . body . nota
-    };
-
-    alunos.push(novoAluno);
-    proximoId ++;
-
-res.status(201).json(novoAluno);
-}) ;
 
 const pintos = [
         {id: 1, nome: "Pinto negro de 35cm", preço: 57.90},
@@ -50,19 +34,44 @@ const pintos = [
     ];
 
 app.get('/pintos-de-borracha', (req, res) => {
-    const pintos = [
-        {id: 1, nome: "Pinto negro de 35cm", preço: 57.90},
-        {id: 2, nome: "Pênis torto para a direita (ou esquerda se mudar o lado) com sebindo abaixo do cabeçote, 50cm", preço: 82.99},
-        {id: 3, nome: "Pica de 10cm azul", preço: 43.00}
-    ];
     res.json(pintos)
 });
+
+    let proxId = 4
+
+app.post('/lanches', (req, res) => {
+
+    const novoLanche = {
+        id : proxId,
+        nome : req.body.nome,
+        preço : req.body.preço
+    }
+    lanches.push(novoLanche);
+    proxId++
+
+    res.status(201).json(novoLanche);
+})
+
+app.put('/pintos-de-borracha/:id', ( req , res ) => {
+
+    const id = Number(req . params . id) ;
+    const pintos = pintos.find(a => a . id === id) ;
+ 
+    if (!pintos) {
+        return res.status(404).json({erro: "Pinto nao encontrado" }) ;
+    }
+
+    pintos.nome = req.body.nome;
+    pintos.preço = req.body.preço;
+
+    res.json(pintos);
+}) ;
 
 app.get('/pintos-de-borracha/:id', (req, res) => {
     const id = Number(req.params.id);
     const penis = pintos.find(a => a.id === id);
 
-    if(!pintos){
+    if(!pintos.id){
         return res.status(404).json({error: "Pênis não encontrado!!!"})
     }
     res.json(penis)
